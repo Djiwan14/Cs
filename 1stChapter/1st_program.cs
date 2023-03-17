ced40236546c0f09609f3fa0d41a6aadd2efcd56
Djiwan14 <118401885+Djiwan14@users.noreply.github.com>
2023-03-17 23:38:41 +0100

    Create 1st_program.cs

---
+++ 1stChapter/1st_program.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Hello_World
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Shokhrukh");
+            Console.Read();
+        }
+    }
+}
